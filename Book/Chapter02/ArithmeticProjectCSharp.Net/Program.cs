6c9da190a9eafe60e74059e5754e15218d4872ad
irmoralesb <irmoralesb@hotmail.com>
2020-01-15 17:12:15 -0600

    Adding Chapter 02 - .Net Project with C#

---
+++ Book/Chapter02/ArithmeticProjectCSharp.Net/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticProjectCSharp.Net
+{
+    class Program
+    {
+        static int a, b;
+        static void Main(string[] args)
+        {
+            a = 1;
+
+            b = 1;
+
+            b = b + a;
+
+            ++a;
+
+            b = a * b;
+
+            return;
+        }
+    }
+}
